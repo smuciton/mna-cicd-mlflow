a08235f0208312e769f9b430e773c9c08e3034b3
smuciton <sebmucito@gmail.com>
2026-09-21 13:07:48 -0600

    equal 4

--- src/train.py
+++ src/train.py
@@ -11,12 +11,12 @@ y = data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-mlflow.set_tracking_uri(uri="https://tinkling-giver-snowsuit.ngrok-free.dev/")
+mlflow.set_tracking_uri(uri="https://tinkling-giver-snowsuit.ngrok-free.dev")
 mlflow.set_experiment("wine-quality-experiment")
 
 with mlflow.start_run():
     # Train a model
-    random_state = 3
+    random_state = 4
     model = RandomForestClassifier(n_estimators=50, random_state=random_state)
     mlflow.log_param("random_state", random_state)
     model.fit(X_train, y_train)
